c122291b1deb39af7622970ce1b6af9ae698746c
Abhinav Karnatak <abhinavkarnatak2004@gmail.com>
2024-12-17 14:05:13 +0530

    Fixed footer mobile responsiveness

--- frontend/app/components/Footer.js
+++ frontend/app/components/Footer.js
@@ -6,10 +6,10 @@ import { FaXTwitter } from "react-icons/fa6";
 const Footer = () => {
   return (
     <div className="w-full h-16 bg-[#19232D] border-t-2 border-[#FFF] flex items-center justify-between px-4">
-      <p className="text-[#FFF] text-sm font-sans font-medium">
+      <p className="text-[#FFF] text-xs lg:text-sm font-sans font-medium">
         © 2024 AviaGeek
       </p>
-      <div className="flex text-xl space-x-12">
+      <div className="flex text-md lg:text-xl space-x-8 lg:space-x-12">
         <Link href="https://www.linkedin.com/in/abhinav-karnatak-9a7667206/">
           <FaLinkedinIn className="text-[#FFF] hover:scale-150 hover:text-[#DCBB87] transition duration-200" />
         </Link>
